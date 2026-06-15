334f62f8ddf095db71fc445637a98e5455ac7b3b
agremi123 <b00459997@essec.edu>
2026-06-16 02:08:39 +0700

    Auto-commit: layout.tsx

--- ecoai/app/layout.tsx
+++ ecoai/app/layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <TikTokPixel />
         <LanguageProvider>{children}</LanguageProvider>
+        <Analytics />
       </body>
     </html>
   );
